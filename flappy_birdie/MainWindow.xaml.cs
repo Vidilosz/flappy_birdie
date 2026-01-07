dcee40d907804eee57043eb7bafa8f64048e3b29
Petrik Diak <diak@petrik.hu>
2026-01-07 09:15:20 +0100

    Új kezdő menü

--- flappy_birdie/MainWindow.xaml.cs
+++ flappy_birdie/MainWindow.xaml.cs
@@ -16,9 +16,52 @@ namespace flappy_birdie
     /// </summary>
     public partial class MainWindow : Window
     {
+
+
         public MainWindow()
         {
             InitializeComponent();
+            Normal.Visibility = Visibility.Collapsed;
+            Rainy_day.Visibility = Visibility.Collapsed;
+            TFI.Visibility = Visibility.Collapsed;
         }
-    }
+
+		private void Start_Click(object sender, RoutedEventArgs e)
+		{
+            Start.Visibility = Visibility.Collapsed;
+			Normal.Visibility = Visibility.Visible;
+			Rainy_day.Visibility = Visibility.Visible;
+			TFI.Visibility = Visibility.Visible;
+
+		}
+
+		private void Normal_Click(object sender, RoutedEventArgs e)
+		{
+			Normal.Visibility = Visibility.Collapsed;
+			Rainy_day.Visibility = Visibility.Collapsed;
+			TFI.Visibility = Visibility.Collapsed;
+			StartGame();
+		}
+
+		private void Rainy_day_Click(object sender, RoutedEventArgs e)
+		{
+			Normal.Visibility = Visibility.Collapsed;
+			Rainy_day.Visibility = Visibility.Collapsed;
+			TFI.Visibility = Visibility.Collapsed;
+			StartGame();
+		}
+
+		private void TFI_Click(object sender, RoutedEventArgs e)
+		{
+			Normal.Visibility = Visibility.Collapsed;
+			Rainy_day.Visibility = Visibility.Collapsed;
+			TFI.Visibility = Visibility.Collapsed;
+			StartGame();
+		}
+
+		public void StartGame()
+		{
+
+		}
+	}
 }
